76324797ef328f10151eac7f7e84ebb3f0212c34
Amit Kumar <dtu.amit@gmail.com>
2020-11-12 18:03:03 +0100

    Remove parallelism from Numba

--- sgkit/distance/metrics.py
+++ sgkit/distance/metrics.py
@@ -25,7 +25,7 @@ N_MAP_PARAM = {
     "(n),(n),(p)->(p)",
     nopython=True,
     cache=True,
-    target="parallel",
+    # target="parallel",
 )
 def correlation_map(x: ArrayLike, y: ArrayLike, _: ArrayLike, out: ArrayLike) -> None:
     """Pearson correlation "map" function for partial vector pairs.
@@ -123,7 +123,7 @@ def correlation_map(x: ArrayLike, y: ArrayLike, _: ArrayLike, out: ArrayLike) ->
     "(p,m)->()",
     nopython=True,
     cache=True,
-    target="parallel",
+    # target="parallel",
 )  # type: ignore
 def correlation_reduce(v: ArrayLike, out: ArrayLike) -> None:
     """Corresponding "reduce" function for pearson correlation
@@ -202,7 +202,7 @@ def correlation_reduce(v: ArrayLike, out: ArrayLike) -> None:
     "(n),(n),(p)->(p)",
     nopython=True,
     cache=True,
-    target="parallel",
+    # target="parallel",
 )
 def euclidean_map(x: ArrayLike, y: ArrayLike, _: ArrayLike, out: ArrayLike) -> None:
     """Euclidean distance "map" function for partial vector pairs.
@@ -278,7 +278,7 @@ def euclidean_map(x: ArrayLike, y: ArrayLike, _: ArrayLike, out: ArrayLike) -> N
     "(p,m)->()",
     nopython=True,
     cache=True,
-    target="parallel",
+    # target="parallel",
 )  # type: ignore
 def euclidean_reduce(v: ArrayLike, out: ArrayLike) -> None:
     """Corresponding "reduce" function
